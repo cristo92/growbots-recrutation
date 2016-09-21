50cf41b8f156f6a7f38be16fbe7cfee8775c9d0e
Krzysztof Leszczynski <leszczynski.christopher@gmail.com>
2016-09-21 23:09:34 +0200

    Create OAuthorisation. Store request_token in session.

--- followers/views.py
+++ followers/views.py
@@ -5,31 +5,30 @@ from django.template import loader
 import tweepy
 
 def index(request):
-	template = loader.get_template('followers/index.html')
-	llist = [i for i in range(0, 10)]
+    template = loader.get_template('followers/index.html')
+    llist = [i for i in range(0, 10)]
 
-	#get oauth from GET
-	consumer_key="XnZcVbUhuLyR0rkgDaXXuNcBC"
-	consumer_secret="yOk9R7xarlWN68S88xP1PqDnX3CGDPndw7gUPKNbIPLGgjNNji"
-	access_token=request.GET.get('oauth_token', None)
-	access_token_secret=request.GET.get('oauth_verifier', None)
+    #get oauth from GET
+    consumer_key="XnZcVbUhuLyR0rkgDaXXuNcBC"
+    consumer_secret="yOk9R7xarlWN68S88xP1PqDnX3CGDPndw7gUPKNbIPLGgjNNji"
+    request_token=request.session.get('request_token', None)
+    verifier_token=request.GET.get('oauth_verifier', None)
 
-	context = { 
-		'basic_list': llist, 
-		'name': None
-	}
+    context = { 
+        'basic_list': llist, 
+        'name': None
+    }
 
-	if(access_token):
+    if(request_token and verifier_token):
 
-		auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-		auth.secure = True
+        auth = tweepy.OAuthHandler(consumer_key, consumer_secret, "http://localhost:8000/followers/followers")
+        auth.secure = True
+        auth.request_token = request_token
 
-		api = tweepy.API(auth)
+        auth.get_access_token(verifier_token)
 
-		auth.set_access_token(access_token, access_token_secret)
+        api = tweepy.API(auth)
 
-		api = tweepy.API(auth)
+        context['name'] = api.me().name
 
-		context['name'] = api.me().name
-
-	return HttpResponse(template.render(context, request))
+    return HttpResponse(template.render(context, request))
--- main/views.py
+++ main/views.py
@@ -5,38 +5,67 @@ from django.template import loader
 import tweepy
 
 def index(request):
-	template = loader.get_template('main/index.html')
+    template = loader.get_template('main/index.html')
 
-	consumer_key="XnZcVbUhuLyR0rkgDaXXuNcBC"
-	consumer_secret="yOk9R7xarlWN68S88xP1PqDnX3CGDPndw7gUPKNbIPLGgjNNji"
+    consumer_key="XnZcVbUhuLyR0rkgDaXXuNcBC"
+    consumer_secret="yOk9R7xarlWN68S88xP1PqDnX3CGDPndw7gUPKNbIPLGgjNNji"
 
-	auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-	auth.secure = True
-	
-	context = { "authorization_url": auth.get_authorization_url() }
-	return HttpResponse(template.render(context, request))
+    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+    auth.secure = True
+    
+    context = { "authorization_url": auth.get_authorization_url() }
+
+    request.session['request_token'] = auth.request_token
+
+    return HttpResponse(template.render(context, request))
 
 def oauthorize():
-	# == OAuth Authentication ==
-	consumer_key="XnZcVbUhuLyR0rkgDaXXuNcBC"
-	consumer_secret="yOk9R7xarlWN68S88xP1PqDnX3CGDPndw7gUPKNbIPLGgjNNji"
+    # == OAuth Authentication ==
+    consumer_key="XnZcVbUhuLyR0rkgDaXXuNcBC"
+    consumer_secret="yOk9R7xarlWN68S88xP1PqDnX3CGDPndw7gUPKNbIPLGgjNNji"
 
-	access_token="778627306789240832-20ELlzX0OYcf41z4Pqinls6Pb3lw3ce"
-	access_token_secret="AMF35NuEhltDdtmzbZGqOrkS74veJBpblajSwtDdXAjJu"
+    access_token="l-gh6QAAAAAAxFSnAAABV05F_V8"
+    access_token_secret="QLmp2CbpVkB27lZf5IjisjRabJdsw8EY"
 
-	auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-	auth.secure = True
-	print auth.get_authorization_url()
-
-
-	"""auth.get_access_token("verifier_value")
+    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+    auth.secure = True
+    print auth.get_authorization_url()
 
 
-	auth.set_access_token(access_token, access_token_secret)
+    auth.get_access_token("verifier_value")
 
-	api = tweepy.API(auth)
 
-	print(api.me().name)
+    auth.set_access_token(access_token, access_token_secret)
 
-	api.update_status(status='Updating using OAuth authentication via Tweepy!')
-"""
+    api = tweepy.API(auth)
+
+    print(api.me().name)
+
+    api.update_status(status='Updating using OAuth authentication via Tweepy!')
+
+def oauthorize2():
+    # == OAuth Authentication ==
+    consumer_key="XnZcVbUhuLyR0rkgDaXXuNcBC"
+    consumer_secret="yOk9R7xarlWN68S88xP1PqDnX3CGDPndw7gUPKNbIPLGgjNNji"
+
+    request_token="l-gh6QAAAAAAxFSnAAABV05F_V8"
+    verifier="QLmp2CbpVkB27lZf5IjisjRabJdsw8EY"
+
+    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+    auth.secure = True
+    print auth.get_authorization_url()
+    auth.request_token=request_token
+
+    try:
+        auth.get_access_token(verifier)
+    except tweepy.TweepError:
+        print 'Error! Failed to get access token.'
+        return None
+
+    auth.set_access_token(auth.access_token, auth.access_token_secret)
+
+    api = tweepy.API(auth)
+
+    print(api.me().name)
+
+    api.update_status(status='Updating using OAuth authentication via Tweepy!')
