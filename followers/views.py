05be08c4ef12905ef0771d9fb3d44a0d7b7dc2a2
Krzysztof Leszczynski <leszczynski.christopher@gmail.com>
2016-09-22 12:29:59 +0200

    Working version 0.01 using Tweety library

--- followers/views.py
+++ followers/views.py
@@ -11,24 +11,44 @@ def index(request):
     #get oauth from GET
     consumer_key="XnZcVbUhuLyR0rkgDaXXuNcBC"
     consumer_secret="yOk9R7xarlWN68S88xP1PqDnX3CGDPndw7gUPKNbIPLGgjNNji"
+
+    access_token = request.session.get('access_token', None)
+    access_token_secret = request.session.get('access_token_secret', None)
+
     request_token=request.session.get('request_token', None)
     verifier_token=request.GET.get('oauth_verifier', None)
 
     context = { 
         'basic_list': llist, 
-        'name': None
+        'name': None,
+        'friends': []
     }
 
-    if(request_token and verifier_token):
+    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+    auth.secure = True
 
-        auth = tweepy.OAuthHandler(consumer_key, consumer_secret, "http://localhost:8000/followers/followers")
-        auth.secure = True
+    api = None
+
+    if(access_token and access_token_secret):
+        auth.set_access_token(access_token, access_token_secret)
+
+        api = tweepy.API(auth)
+
+    elif(request_token and verifier_token):
         auth.request_token = request_token
 
         auth.get_access_token(verifier_token)
 
-        api = tweepy.API(auth)
+        request.session['access_token'] = auth.access_token
+        request.session['access_token_secret'] = auth.access_token_secret
 
+        api = tweepy.API(auth)
+    
+    if(api):
+        username = auth.get_username()
         context['name'] = api.me().name
+
+        friends = api.friends_ids(username)
+        context['friends'] = [api.get_user(x).screen_name for x in friends]
 
     return HttpResponse(template.render(context, request))
